cb600b436d2f51c6f6e0012438a7ae23fb5fff5b
eriteria <joseph.john.ali.2001@gmail.com>
2021-07-16 01:47:43 +0100

    First Commit

--- accoms/settings.py
+++ accoms/settings.py
@@ -1,4 +1,7 @@
+import os
 FLASK_DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:////home/joseph/PycharmProjects/accoms/accoms/db.sqlite3'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:////home/joseph/PycharmProjects/accoms/accoms/db.sqlite3'
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'mysecretkey'
